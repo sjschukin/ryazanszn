670cf5b92fec8fdcd0ed83b0e2dd704c9b79d8e2
Sergey Shchukin <s.j.schukin@yandex.ru>
2020-11-01 20:16:44 +0300

    added global search panel to header

--- web/themes/blondytan2/js/global.js
+++ web/themes/blondytan2/js/global.js
@@ -3,11 +3,8 @@
  */
 (function ($) {
   "use strict";
-  // All the JavaScript for this file.
 
-  // ===================================
-  // blind version                     =
-  // ===================================
+  // blind version *********************
   $('#to-blind-version').on('click', function () {
     SetBlindStyle();
     $.cookie('blindVersion', 1, { expires: 7, path: '/'});
@@ -58,7 +55,7 @@
   }
 
   // to top navigation *********************
-  $("#back-top").hide();
+  $('#back-top').hide();
 
   $(function () {
     $(window).scroll(function () {
@@ -76,90 +73,19 @@
       return false;
     });
   });
+
+  // search navigation panel *********************
+  $('#nav-search-row').hide();
+
+  $('#search-button').click(function () {
+    $('#nav-menu-row').hide();
+    $('#nav-search-row').hide().removeClass('hidden');
+    $('#nav-search-row').fadeIn('slow');
+  });
+
+  $('#search-cancel').click(function () {
+    $('#nav-search-row').fadeOut('slow', function () {
+      $('#nav-menu-row').show();
+    });
+  });
 })(jQuery);
-
-
-
-/*
-$(function () {
-  // Blind panel
-  $('.js-toggle_fontsize').on('click', function () {
-    $(this).siblings().removeClass('selected').end().addClass('selected');
-    var bodyClass = $(this).attr('data-class');
-    $('body').removeClass('fontsize-small fontsize-middle fontsize-big').addClass(bodyClass);
-  });
-  $('.js-toggle_color').on('click', function () {
-    $(this).siblings().removeClass('selected').end().addClass('selected');
-    var bodyClass = $(this).attr('data-class');
-    $('body').removeClass('color_white color_black color_blue').addClass(bodyClass);
-  });
-  $('.js-toggle_img').on('click', function () {
-    $(this).siblings().removeClass('selected').end().addClass('selected');
-    var bodyClass = $(this).attr('data-class');
-    $('body').removeClass('image_off image_on').addClass(bodyClass);
-  });
-  $('.js-toggle_font').on('click', function () {
-    $(this).siblings().removeClass('selected').end().addClass('selected');
-    var bodyClass = $(this).attr('data-class');
-    $('body').removeClass('sans_serif serif').addClass(bodyClass);
-  });
-  $('.js-interlinear').on('click', function () {
-    $(this).siblings().removeClass('selected').end().addClass('selected');
-    var bodyClass = $(this).attr('data-class');
-    $('body').removeClass('standart_interlinear middle_interlinear big_interlinear').addClass(bodyClass);
-  });
-  $('.settings_ico').on('click', function () {
-    $('.settings_block').hide();
-    $(this).parents('.blind_panel').find('.settings_block').show();
-    $('.js-close_sets').on('click', function () {
-      $('.settings_block').hide();
-    });
-  });
-  $('.js-default_set').on('click', function () {
-    var bodyClass = 'sans_serif fontsize-small standart_interlinear color_white image_on';
-    $('body').removeAttr('class').addClass(bodyClass);
-    $('.js-toggle_fontsize, .js-toggle_color, .js-toggle_img').removeClass('selected');
-    $('[data-class = sans_serif]').addClass('selected');
-    $('[data-class = fontsize-small]').addClass('selected');
-    $('[data-class = standart_interlinear]').addClass('selected');
-    $('[data-class = color_white]').addClass('selected');
-    $('[data-class = image_on]').addClass('selected');
-  });
-  function SetBlindStyle() {
-    $('body').addClass('sans_serif fontsize-small standart_interlinear color_white image_on js-blind blind');
-    $('#page_wrapper').removeClass('css_main').addClass('css_blind');
-    $('#foot_wrap').removeClass('css_main').addClass('css_blind');
-  }
-  function SetMainStyle() {
-    $('body').removeClass('sans_serif fontsize-small standart_interlinear color_white image_on js-blind blind');
-    $('#page_wrapper').removeClass('css_blind').addClass('css_main');
-    $('#foot_wrap').removeClass('css_blind').addClass('css_main');
-  }
-
-  var blindVersion;
-  $('#to_blind_version').on('click', function () {
-    SetBlindStyle();
-    $masonryContainer.masonry();
-    $.cookie('blindVersion', 1, {
-      expires: 7,
-      path: '/'
-    });
-    return false;
-  });
-  $('.to_main_version').on('click', function () {
-    SetMainStyle();
-    $masonryContainer.masonry();
-    $.cookie('blindVersion', 0, {
-      expires: 7,
-      path: '/'
-    });
-    return false;
-  });
-  var blind = $.cookie('blindVersion');
-  if (blind == 1) {
-    SetBlindStyle();
-  } else {
-    SetMainStyle();
-  }
-});
-*/
