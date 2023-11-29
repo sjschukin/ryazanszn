5726cf7995f6529877b7b8c10d1eebef0b865ac1
Sergey Shchukin <s.j.schukin@yandex.ru>
2023-11-30 00:45:47 +0300

    restored blind version

--- web/themes/blondytan2/js/global.js
+++ web/themes/blondytan2/js/global.js
@@ -2,56 +2,86 @@
  * @file
  */
 
-(function (Drupal, cookies) {
+(function (Drupal) {
   "use strict";
 
-  /*Drupal.behaviors.blindVersionBehavior = {
+  Drupal.behaviors.blindVersionBehavior = {
     attach: () => {
-      let blind = $.cookie('blindVersion');
+      let blind = getCookie('blindVersion');
 
       if (blind == 1) {
-        SetBlindStyle();
+        setBlindStyle();
       } else {
-        SetMainStyle();
+        setMainStyle();
       }
 
       document.querySelector('#to-blind-version').addEventListener('click', () => {
-        SetBlindStyle();
-        cookies.set('blindVersion', 1, JSON.stringify({ expires: 7, path: '/'}));
-
+        setBlindStyle();
+        setCookie('blindVersion', 1);
         return false;
       });
 
       document.querySelector('.to-main-version').addEventListener('click', () => {
-        SetMainStyle();
-        cookies.set('blindVersion', 0, JSON.stringify({ expires: 7, path: '/'}));
-
+        setMainStyle();
+        setCookie('blindVersion', 0);
         return false;
       });
+
+      let toggleFontSizeElements = document.querySelectorAll('.font-toggle > .js-toggle-fontsize');
+      let toggleColorElements = document.querySelectorAll('.color-toggle > .js-toggle-color');
+      let toggleImageElements = document.querySelectorAll('.image-toggle > .js-toggle-img');
+
+      for (let i = 0; i < toggleFontSizeElements.length; i++) {
+        toggleFontSizeElements[i].addEventListener('click', function () {
+          for (let j = 0; j < toggleFontSizeElements.length; j++) {
+            toggleFontSizeElements[j].classList.remove('selected');
+          }
+
+          this.classList.add('selected');
+
+          let bodyClass = this.getAttribute('data-class');
+          document.body.classList.remove('fontsize-small', 'fontsize-middle', 'fontsize-big');
+          document.body.classList.add(bodyClass);
+          setCookie('blindFontSize', bodyClass);
+          return false;
+        });
+      }
+
+      for (let i = 0; i < toggleColorElements.length; i++) {
+        toggleColorElements[i].addEventListener('click', function () {
+          for (let j = 0; j < toggleColorElements.length; j++) {
+            toggleColorElements[j].classList.remove('selected');
+          }
+
+          this.classList.add('selected');
+
+          let bodyClass = this.getAttribute('data-class');
+          document.body.classList.remove('color-white', 'color-black', 'color-blue');
+          document.body.classList.add(bodyClass);
+          setCookie('blindColor', bodyClass);
+          return false;
+        });
+      }
+
+      for (let i = 0; i < toggleImageElements.length; i++) {
+        toggleImageElements[i].addEventListener('click', function () {
+          for (let j = 0; j < toggleImageElements.length; j++) {
+            toggleImageElements[j].classList.remove('selected');
+          }
+
+          this.classList.add('selected');
+
+          let bodyClass = this.getAttribute('data-class');
+          document.body.classList.remove('image-off', 'image-on');
+          document.body.classList.add(bodyClass);
+          setCookie('blindImage', bodyClass);
+          return false;
+        });
+      }
     },
   };
 
-  // blind version *********************
-  // $('#to-blind-version').on('click', function () {
-  //   SetBlindStyle();
-  //   $.cookie('blindVersion', 1, { expires: 7, path: '/'});
-  //   return false;
-  // });
-
-  // $('.to-main-version').on('click', function () {
-  //   SetMainStyle();
-  //   $.cookie('blindVersion', 0, { expires: 7, path: '/'});
-  //   return false;
-  // });
-
-  // var blind = $.cookie('blindVersion');
-  // if (blind == 1) {
-  //   SetBlindStyle();
-  // } else {
-  //   SetMainStyle();
-  // }
-
-  $('.js-toggle-fontsize').on('click', function () {
+  /*$('.js-toggle-fontsize').on('click', function () {
     $(this).siblings().removeClass('selected').end().addClass('selected');
     var bodyClass = $(this).attr('data-class');
     $('body').removeClass('fontsize-small fontsize-middle fontsize-big').addClass(bodyClass);
@@ -68,18 +98,40 @@
     var bodyClass = $(this).attr('data-class');
     $('body').removeClass('image-off image-on').addClass(bodyClass);
     $.cookie('blind_image', bodyClass, { expires: 7, path: '/'});
-  });
+  });*/
 
-  function SetBlindStyle() {
-    $('body').addClass('fontsize-small color-white image-on css-blind');
+  function setBlindStyle() {
+    document.body.classList.add('fontsize-small','color-white','image-on','css-blind');
     //$('#page-wrapper').addClass('css-blind');
     //$('#foot_wrap').removeClass('css_main').addClass('css_blind');
   }
-  function SetMainStyle() {
-    $('body').removeClass('fontsize-small fontsize-middle fontsize-big color-white color-black color-blue css-blind');
+
+  function setMainStyle() {
+    document.body.classList.remove('fontsize-small','fontsize-middle','fontsize-big','color-white','color-black','color-blue','css-blind');
     //$('#page-wrapper').removeClass('css-blind');
     //$('#foot_wrap').removeClass('css_blind').addClass('css_main');
-  } */
+  }
+
+  function setCookie(name, value) {
+    let date = new Date;
+    date.setTime(date.getTime() + 24 * 60 * 60 * 1000 * 7);
+    document.cookie = name + '=' + value + ';path=/;expires=' + date.toUTCString();
+  }
+
+  function getCookie(name) {
+    name = name + '=';
+
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookies = decodedCookie.split(';');
+
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+
+      if (cookie.indexOf(name) == 0) {
+        return cookie.substring(name.length, cookie.length);
+      }
+    }
+  }
 
   // to the top navigation *********************
   let backTop = document.getElementById('back-top');
